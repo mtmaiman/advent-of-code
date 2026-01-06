d8bbb9136f0a50a4b9d1c502e68f96aa7d3fe67b
Matt Maiman <mmaiman@aca.aaa.com>
2026-01-06 10:06:24 -0500

    day 4 part 2

--- day-4/day-4.py
+++ day-4/day-4.py
@@ -1,40 +1,50 @@
 import numpy as np
 
-input = open('day-4.input').read()
-grid = np.array([list(row) for row in input.split('\n')])
-marked = np.zeros(shape = (grid.shape[0], grid.shape[1]), dtype = str)
-accessible = 0
+def my_solution(grid, removed = 0, initial = False):
+    accessible = 0
 
-for coordinate, roll in np.ndenumerate(grid):
-    if (roll != '@'):
-        marked[coordinate[0]][coordinate[1]] = grid[coordinate[0]][coordinate[1]]
-        continue
-
-    neighbors = [
-        (coordinate[0] - 1, coordinate[1] - 1), # top left
-        (coordinate[0] - 1, coordinate[1]), # top center
-        (coordinate[0] - 1, coordinate[1] + 1), # top right
-        (coordinate[0], coordinate[1] - 1), # adjacent left
-        (coordinate[0], coordinate[1] + 1), # adjcent right
-        (coordinate[0] + 1, coordinate[1] - 1), # bottom left
-        (coordinate[0] + 1, coordinate[1]), # bottom center
-        (coordinate[0] + 1, coordinate[1] + 1) # bottom right
-    ]
-
-    occupied = 0
-
-    for neighbor in neighbors:
-        if (neighbor[0] < 0 or neighbor[0] >= grid.shape[0] or neighbor[1] < 0 or neighbor[1] >= grid.shape[1]): # check bounds of neighbor
+    for coordinate, roll in np.ndenumerate(grid):
+        if (roll != '@'):
             continue
 
-        if (grid[neighbor[0]][neighbor[1]] == '@'):
-            occupied = occupied + 1
+        neighbors = [
+            (coordinate[0] - 1, coordinate[1] - 1), # top left
+            (coordinate[0] - 1, coordinate[1]), # top center
+            (coordinate[0] - 1, coordinate[1] + 1), # top right
+            (coordinate[0], coordinate[1] - 1), # adjacent left
+            (coordinate[0], coordinate[1] + 1), # adjcent right
+            (coordinate[0] + 1, coordinate[1] - 1), # bottom left
+            (coordinate[0] + 1, coordinate[1]), # bottom center
+            (coordinate[0] + 1, coordinate[1] + 1) # bottom right
+        ]
 
-    if (occupied < 4):
-        marked[coordinate[0]][coordinate[1]] = 'X'
-        accessible = accessible + 1
+        occupied = 0
+
+        for neighbor in neighbors:
+            if (neighbor[0] < 0 or neighbor[0] >= grid.shape[0] or neighbor[1] < 0 or neighbor[1] >= grid.shape[1]): # check bounds of neighbor
+                continue
+
+            if (grid[neighbor[0]][neighbor[1]] == '@'):
+                occupied = occupied + 1
+
+        if (occupied < 4):
+            grid[coordinate[0]][coordinate[1]] = '.'
+            accessible = accessible + 1
+        else:
+            grid[coordinate[0]][coordinate[1]] = roll
+
+    if (initial):
+        print(accessible)
+
+    if (accessible == 0):
+        return removed
     else:
-        marked[coordinate[0]][coordinate[1]] = roll
+        removed += accessible
+        return my_solution(grid, removed = removed)
 
-print(''.join(''.join(row) + '\n' for row in marked).strip())
-print(accessible)
+
+file = 'day-4.input'
+input = open(file).read()
+grid = np.array([list(row) for row in input.split('\n')])
+removed = my_solution(grid, initial = True)
+print(removed)
